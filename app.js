9671a60ee7c55659059b33214e6643e05945c99d
Your Name <you@example.com>
2023-08-30 18:35:45 -0400

    Created new file for messages

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const nameSelector = () => {
     }
 }
 
-const beginPhrase = ['you are a very focused and determined person', 'you have a fiery and spontaneous personality', 'you are a sincere guenuine person', 'you will learn from your past transgressions',  'you will overcome your dark days']
-const endPhrase = ['and that will pay dividends in the near future.', 'you provide a spark for those around you.', 'the people in your life appreciate that about you.', 'and that will be the foundation of future success.', 'but youre still a loser.']
+import {beginPhrase} from './messages'
+import {endPhrase} from './messages'
 
 const messageGenerator = () => {
     const randBeginIndex = Math.floor(Math.random() * 5)
