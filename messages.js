e5e728ed1883a85f9d1555c6f464346fde65f87d
Your Name <you@example.com>
2023-08-30 18:59:42 -0400

    removed transfered messages back to app.js

--- messages.js
+++ messages.js
@@ -1,3 +1,2 @@
-export const beginPhrase = ['you are a very focused and determined person', 'you have a fiery and spontaneous personality', 'you are a sincere guenuine person', 'you will learn from your past transgressions',  'you will overcome your dark days']
-export const endPhrase = ['and that will pay dividends in the near future.', 'you provide a spark for those around you.', 'the people in your life appreciate that about you.', 'and that will be the foundation of future success.', 'but youre still a loser.']
+
 
